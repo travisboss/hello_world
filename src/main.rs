09a140a0f6de99ff40f9c63086ff06076b1d8480
travisboss <travisboss@gmail.com>
2019-06-21 13:39:25 -0500

    added scope

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
 fn main() {
     let mut s = String::from("hello");
 
-    s.push_str(", world!"); // pus_str() appends a literal to a String
+    s.push_str(", world!"); // push_str() appends a literal to a String
 
     println!("{}", s); // This will print 'hello, world!'
+
+    hello_world_clone();
+}
+
+// Using clone to achieve the same thing with variables and data interact
+
+fn hello_world_clone() {
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    hello_world_scope();
+}
+
+fn hello_world_scope() {
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+
+    (s, length)
 }
---
+++ target/debug/deps/hello_world-31fae139081b550f.d
@@ -0,0 +1,5 @@
+/Users/tboss/Code/rust/hello_world/target/debug/deps/hello_world-31fae139081b550f.rmeta: src/main.rs
+
+/Users/tboss/Code/rust/hello_world/target/debug/deps/hello_world-31fae139081b550f.d: src/main.rs
+
+src/main.rs:
